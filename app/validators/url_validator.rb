4ba242e777d4001aad4e323fb0a26c98f5ee3e8a
danlewis <flightofdan@gmail.com>
2017-10-31 14:43:57 +1100

    fix issue with url validator

--- app/validators/url_validator.rb
+++ app/validators/url_validator.rb
@@ -4,13 +4,13 @@ class URLValidator < ActiveModel::EachValidator
     if options[:hostname]
       record.errors.add attribute, "must include #{options[:hostname]}" unless value.include?(options[:hostname])
     end
-    record.errors.add attribute, 'must be a valid url' unless url_valid?(value)
+    record.errors.add attribute, "must be a valid url" unless url_valid?(value)
   end
 
   private
 
   def url_valid?(url)
-    re = /(\A\z)|(\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[:a-z]{2,5}(([0-9]{1,5})?\/.*)?\z)/ix
+    re = /(\A\z)|(\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[:a-z]{2,15}(([0-9]{1,5})?\/.*)?\z)/ix
     url =~ re
   end
 
