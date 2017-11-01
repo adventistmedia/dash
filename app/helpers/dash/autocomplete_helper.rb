071a527b0b63759f570ab6b7ccfa1c2ea9c39456
danlewis <flightofdan@gmail.com>
2017-11-02 09:15:41 +1100

    fix autocomplete from browser

--- app/helpers/dash/autocomplete_helper.rb
+++ app/helpers/dash/autocomplete_helper.rb
@@ -4,7 +4,7 @@ module Dash::AutocompleteHelper
     id = "ia-#{rand(99999999)}"
     content_tag(:div, class: "input string ia-wrapper") do
       content_tag(:label, options[:label], class: "control-label") +
-      text_field_tag(:search, options[:value], placeholder: options[:placeholder], data: {url: options[:url], target: options[:target]}, id: id, autocomplete: "false", class: "form-control input-autocomplete") +
+      text_field_tag(:search, options[:value], placeholder: options[:placeholder], data: {url: options[:url], target: options[:target]}, id: id, autocomplete: "off", class: "form-control input-autocomplete") +
       content_tag(:ul, "", id: "#{id}-results", class: "ia-results")
     end
   end
