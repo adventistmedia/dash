6f687b825951cb374ed6b3a7d4cdd587149e24c1
danlewis <flightofdan@gmail.com>
2017-11-08 11:32:06 +1100

    update social icons ui

--- app/helpers/dash/social_helper.rb
+++ app/helpers/dash/social_helper.rb
@@ -1,9 +1,11 @@
 module Dash::SocialHelper
 
   def social_icon(platform)
-    case platform
+    case platform.to_s
     when "website"
       "share-alt"
+    when "youtube"
+      "youtube-play"
     else
       platform
     end
