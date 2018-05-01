725dd45ce9414464bd6d77df842a42549f493a2f
danlewis <flightofdan@gmail.com>
2018-05-01 14:37:45 +1000

    fix bug

--- app/helpers/dash/support_helper.rb
+++ app/helpers/dash/support_helper.rb
@@ -13,7 +13,7 @@ module Dash::SupportHelper
     beacon_custom_config = %Q{HS.beacon.config({topArticles: true,poweredBy: false, topics: [{val: "help", label: "Help"}, {val: "feature-request", label: "Feature request"}, {val: "bug", label: "Report a bug"}]});}
     beacon_base_js = %Q{!function(e,o,n){window.HSCW=o,window.HS=n,n.beacon=n.beacon||{};var t=n.beacon;t.userConfig={},t.readyQueue=[],t.config=function(e){this.userConfig=e},t.ready=function(e){this.readyQueue.push(e)},o.config=#{beacon_config};var r=e.getElementsByTagName("script")[0],c=e.createElement("script");c.type="text/javascript",c.async=!0,c.src="https://djtflbt20bdde.cloudfront.net/",r.parentNode.insertBefore(c,r)}(document,window.HSCW||{},window.HS||{});}
     beacon_options = []
-    beacon_options << %Q{HS.beacon.prefill({subject: "#{Rails.application.credentials.dig(:helpscout, :contact_form_subject}"});}
+    beacon_options << %Q{HS.beacon.prefill({subject: "#{Rails.application.credentials.dig(:helpscout, :contact_form_subject)}"});}
     if @current_user
       beacon_options << %Q{HS.beacon.identify({name: "#{@current_user.name}", email: "#{@current_user.email}"});}
     end
